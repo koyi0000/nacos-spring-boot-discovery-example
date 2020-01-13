9e53f2acb5c5f6ee28c4716e413a165137e29d24
gyj@yibaotong.top <gyj@yibaotong.top>
2020-01-13 20:25:14 +0800

    build nacos discovery

---
+++ src/test/java/com/nacos/spring/boot/discovery/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nacos.spring.boot.discovery.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
